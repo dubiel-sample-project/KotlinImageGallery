59d2c44654aae144e4c96e72b347a3ee8f87c2ed
Matthias Dubiel <matthias.dubiel@pwc-digital.ch>
2018-04-22 16:09:25 +0200

    changed to RecyclerView layout for grid

--- app/src/main/java/com/dubiel/sample/kotlinimagegallery/MainActivity.kt
+++ app/src/main/java/com/dubiel/sample/kotlinimagegallery/MainActivity.kt
@@ -2,45 +2,59 @@ package com.dubiel.sample.kotlinimagegallery
 
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
-import android.util.Log
 import android.widget.AdapterView
 import android.widget.GridView
-import android.widget.Toast
-import com.dubiel.sample.kotlinimagegallery.retrofit.GalleryImages
 import com.dubiel.sample.kotlinimagegallery.retrofit.ImageGalleryClient
-import rx.Subscription
 import rx.android.schedulers.AndroidSchedulers
 import rx.schedulers.Schedulers
+import android.support.v7.widget.GridLayoutManager
+import android.support.v7.widget.RecyclerView
+
 
 data class GalleryImage(var name : String)
 data class GalleryImages(var items: Array<GalleryImage>)
 
 class MainActivity : AppCompatActivity() {
     val TAG = MainActivity::class.java.getSimpleName()
-
-    var subscription: Subscription? = null
+//    lateinit var gridView: GridView
+//    lateinit var gridView: GridView
+//    var subscription: Subscription? = null
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        val imageGalleryClient = ImageGalleryClient(applicationContext)
+        val layoutManager = GridLayoutManager(this, 3)
+        layoutManager.spanSizeLookup = object : GridLayoutManager.SpanSizeLookup() {
+            override fun getSpanSize(position: Int): Int {
+                return if (position == 0) 2 else 1
+            }
+        }
 
-        subscription = imageGalleryClient.getImages()
+        ImageGalleryClient(this).getImages()
                 .subscribeOn(Schedulers.io())
                 .observeOn(AndroidSchedulers.mainThread())
                 .subscribe({ images ->
-                    images.items.forEach { image -> System.out.println(image.name) }
+//                    val gridView: GridView = findViewById(R.id.gridview)
+                    val recyclerView : RecyclerView = findViewById(R.id.recyclerview)
+                    recyclerView.layoutManager = layoutManager
+                    recyclerView.adapter = ImageGalleryViewAdapter(this, images.items)
+//                    gridView = findViewById(R.id.gridview)
+//                    gridView.adapter = ImageAdapter(this, images.items)
+//
+//                    recyclerView.on
+//                            AdapterView.OnItemClickListener { parent, v, position, id ->
+//                                Toast.makeText(this, "$position", Toast.LENGTH_SHORT).show()
+//                            }
+//                    gridView.onItemClickListener =
+//                            AdapterView.OnItemClickListener { parent, v, position, id ->
+//                                Toast.makeText(this, "$position", Toast.LENGTH_SHORT).show()
+//                            }
+//                    imageAdapter.notifyDataSetChanged()
 //                    System.out.println(images.javaClass.toString())
 //                    System.out.println(images.items.size)
                 })
 
-        val gridView: GridView = findViewById(R.id.gridview)
-        gridView.adapter = ImageAdapter(this)
 
-        gridView.onItemClickListener =
-                AdapterView.OnItemClickListener { parent, v, position, id ->
-                    Toast.makeText(this, "$position", Toast.LENGTH_SHORT).show()
-                }
     }
 }
