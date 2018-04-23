b432524dba18ddd5e9a42538b574762135d5f5aa
Matthias Dubiel <matthias.dubiel@pwc-digital.ch>
2018-04-23 22:24:18 +0200

    added private fun getGridItem

--- app/src/main/java/com/dubiel/sample/kotlinimagegallery/MainActivity.kt
+++ app/src/main/java/com/dubiel/sample/kotlinimagegallery/MainActivity.kt
@@ -2,62 +2,79 @@ package com.dubiel.sample.kotlinimagegallery
 
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
-import android.widget.AdapterView
-import android.widget.GridView
 import com.dubiel.sample.kotlinimagegallery.retrofit.ImageGalleryClient
 import rx.android.schedulers.AndroidSchedulers
 import rx.schedulers.Schedulers
-import android.support.v7.widget.GridLayoutManager
-import android.support.v7.widget.RecyclerView
-import android.widget.GridLayout
-
+//import android.support.v7.widget.GridLayoutManager
+//import android.support.v7.widget.RecyclerView
+import android.view.LayoutInflater
+import android.view.View
+import android.support.v7.widget.GridLayout
+import android.widget.ImageView
+import android.view.Gravity
+import android.widget.ScrollView
 
 data class GalleryImage(var name : String)
 data class GalleryImages(var items: Array<GalleryImage>)
 
 class MainActivity : AppCompatActivity() {
     val TAG = MainActivity::class.java.getSimpleName()
-//    lateinit var gridView: GridView
-//    lateinit var gridView: GridView
+    lateinit var gridLayout: GridLayout
 //    var subscription: Subscription? = null
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        val gl : GridLayout = findViewById(R.id.grid_layout_main)
+        val scrollView: ScrollView = findViewById(R.id.scroll_view)
+        scrollView.setSmoothScrollingEnabled(true)
 
-//        val layoutManager = GridLayoutManager(this, 3)
-//        layoutManager.spanSizeLookup = object : GridLayoutManager.SpanSizeLookup() {
-//            override fun getSpanSize(position: Int): Int {
-//                return if (position == 0) 2 else 1
-//            }
-//        }
+        gridLayout = findViewById(R.id.grid_layout)
 
         ImageGalleryClient(this).getImages()
                 .subscribeOn(Schedulers.io())
                 .observeOn(AndroidSchedulers.mainThread())
                 .subscribe({ images ->
-//                    val gridView: GridView = findViewById(R.id.gridview)
-//                    val recyclerView : RecyclerView = findViewById(R.id.recyclerview)
-//                    recyclerView.layoutManager = layoutManager
-//                    recyclerView.adapter = ImageGalleryViewAdapter(this, images.items)
-//                    gridView = findViewById(R.id.gridview)
-//                    gridView.adapter = ImageAdapter(this, images.items)
-//
-//                    recyclerView.on
-//                            AdapterView.OnItemClickListener { parent, v, position, id ->
-//                                Toast.makeText(this, "$position", Toast.LENGTH_SHORT).show()
-//                            }
-//                    gridView.onItemClickListener =
-//                            AdapterView.OnItemClickListener { parent, v, position, id ->
-//                                Toast.makeText(this, "$position", Toast.LENGTH_SHORT).show()
-//                            }
-//                    imageAdapter.notifyDataSetChanged()
+                    val view = getGridItem(images.items.first().name,
+                            resources.displayMetrics.widthPixels / 3 * 2,
+                            null,
+                            2,
+                            2)
+                    gridLayout.addView(view)
+
+                    images.items.drop(1).forEach { image ->
+                        gridLayout.addView(getGridItem(image.name,
+                                resources.displayMetrics.widthPixels / 3,
+                                view.layoutParams.height / 2,
+                                1,
+                                1))
+                    }
 //                    System.out.println(images.javaClass.toString())
 //                    System.out.println(images.items.size)
                 })
+    }
 
+    fun getGridItem(name: String, width: Int, height: Int?, rowspan: Int, colspan: Int) : View  {
+        val inflater : LayoutInflater = LayoutInflater.from(this)
+        val itemView : View = inflater.inflate(R.layout.grid_item, gridLayout, false)
 
+        val imageView : ImageView = itemView.findViewById(R.id.image_view)
+        imageView.setImageResource(resources.getIdentifier(name, "drawable", packageName))
+
+        var param = GridLayout.LayoutParams()
+//        param.width = GridLayout.LayoutParams.WRAP_CONTENT
+        param.width = width
+//        param.height = GridLayout.LayoutParams.WRAP_CONTENT
+        if(height != null) {
+            param.height = height
+        }
+        param.rightMargin = 5
+        param.topMargin = 5
+        param.setGravity(Gravity.CENTER)
+        param.rowSpec = GridLayout.spec(GridLayout.UNDEFINED, rowspan)
+        param.columnSpec = GridLayout.spec(GridLayout.UNDEFINED, colspan)
+        itemView.setLayoutParams(param)
+
+        return itemView
     }
 }
